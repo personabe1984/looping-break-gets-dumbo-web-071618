b3a5bc939f9a78354dcabee0652b6e6633770670
Hanbi Hans Choi <personabe1984@gmail.com>
2018-06-30 17:12:14 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,8 +2,10 @@
 def levitation_quiz
 	#your code here
 	loop do 
-	puts "What is the spell that enacts levitation?"
-	answer = gets.chomp
+	  puts "What is the spell that enacts levitation?"
+	  answer = gets.chomp
+	  break if answer == 
+	 end
 end
 
 
